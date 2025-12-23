a8df55f4d4a742cd8f295567a7dee209df18c9c3
tsogtb <circl3t@gmail.com>
2025-12-23 10:36:12 +0100

    setup test environment and lock dependencies

---
+++ vitest.config.js
@@ -0,0 +1,13 @@
+export default defineConfig({
+  test: {
+    // This tells Vitest where to look by default
+    include: ['test/**/*.unit.test.js'],
+    
+    // This allows you to use 'test', 'expect', etc. without importing them 
+    // in every single file if you prefer (optional)
+    globals: true, 
+    
+    // Environment 'node' is fast for math, 'jsdom' is for browser-like tests
+    environment: 'node' 
+  }
+})
